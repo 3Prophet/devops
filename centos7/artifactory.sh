c2b2e9f631d056a08a6ec5a9b6d37f0638ced3d9
Dmitry Logvinovich <dlogvinovich@yahoo.com>
2019-01-24 11:35:03 +0000

    Community version of artifactory

--- centos7/artifactory.sh
+++ centos7/artifactory.sh
@@ -7,9 +7,9 @@ sudo yum install yum install java-1.8.0-openjdk-devel.x86_64 -y
 sudo echo -e "export JAVA_HOME=/usr/lib/jvm/jre-1.8.0-openjdk" >> /etc/profile
 sudo echo -e "export JRE_HOME=/usr/lib/jvm/jre" >> /etc/profile
 sudo source /etc/profile
-sudo wget https://bintray.com/jfrog/artifactory-pro-rpms/rpm -O bintray-jfrog-artifactory-pro-rpms.repo
-sudo mv bintray-jfrog-artifactory-pro-rpms.repo /etc/yum.repos.d/
-sudo yum install jfrog-artifactory-pro -y
+sudo wget https://bintray.com/jfrog/artifactory-rpms/rpm -O bintray-jfrog-artifactory-rpms.repo
+sudo mv bintray-jfrog-artifactory-rpms.repo /etc/yum.repos.d
+sudo yum install jfrog-artifactory-oss.noarch -y
 sudo service artifactory start
 sudo echo "Artifactory is running under: hostname:8081/artifactory"
 #sudo /opt/jfrog/artifactory/bin/artifactoryManage.sh start
